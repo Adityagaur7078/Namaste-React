62bf58dffc22f136d94fb1c57b4edb1b0d222fbc
Adityagaur7078 <adityagaur7078@gmail.com>
2026-03-29 00:14:39 +0530

    feat: add top rated filter and improve header layout with search and button alignment

--- Small Project/Componenets/Body.js
+++ Small Project/Componenets/Body.js
@@ -4,43 +4,59 @@ import resList from "../utils/mockdata";
 
 const Body = () => {
   const [searchText, setSearchText] = useState("");
-  const [filteredList, setFilteredList] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
 
+  // 🔍 Search Function
   const handleSearch = () => {
     const filtered = resList.filter((res) =>
       res.info.name.toLowerCase().includes(searchText.toLowerCase())
     );
-
-    setFilteredList(filtered);
+    setListOfRestaurants(filtered);
   };
 
   return (
     <div className="body">
 
-      {/* 🔍 Search Bar */}
-      <div className="search-container">
-        <input
-          type="text"
-          placeholder="Search restaurant..."
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
-          className="search-input"
-        />
+      {/* ⭐ Top Bar (Filter + Search) */}
+      <div className="top-bar">
 
-        <button onClick={handleSearch} className="search-btn">
-          Search
-        </button>
+        {/* ⭐ Filter Button */}
+        <div className="filter">
+          <button
+            className="btn-filter"
+            onClick={() => {
+              const topRated = resList.filter(
+                (res) => res.info.avgRating >= 4.5
+              );
+              setListOfRestaurants(topRated);
+            }}
+          >
+            Top Rated Restaurant
+          </button>
+        </div>
+
+        {/* 🔍 Search Bar */}
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="Search restaurant..."
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            className="search-input"
+          />
+
+          <button onClick={handleSearch} className="search-btn">
+            Search
+          </button>
+        </div>
+
       </div>
-
-
-
 
 {/* not using keys (not acceptable) <<<< index as key <<<< unique id (best practice) */}
 
-
       {/* 🍔 Restaurant Cards */}
       <div className="restaurant-container">
-        {filteredList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard
             key={restaurant.info.id}
             resData={restaurant}
